8fa5321e2d5f7b244b1d74bfe15b456755d51bf3
Suraj Karan Vejandla <110095400+surajkaran69@users.noreply.github.com>
2022-11-18 20:23:09 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 mkdir -p ~/.streamlit/
-
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"surajkaran.1998@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
